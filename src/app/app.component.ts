ad02c33b762d7ec782d0b7c4205542e3b210b3fc
Victoria AF <fafascinada@gmail.com>
2025-02-18 13:20:35 +0100

    añadido ciclo de vida ngOnInit. el array se rellena cuando se monta el componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { BlogComponent } from "./component/blog/blog.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BlogComponent],
+  imports: [BlogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
