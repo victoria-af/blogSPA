a13fe22c36442670f1d8df5f3636f56433eaacac
Victoria AF <fafascinada@gmail.com>
2025-02-20 11:45:01 +0100

    transformacion del array a multidimensional. Creado objeto de noticia. Recorrido actualizado del array multidimensional en el html

--- src/app/component/blog/blog.component.ts
+++ src/app/component/blog/blog.component.ts
@@ -11,13 +11,13 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent implements OnInit{
-  newsArray: INews[] = []
-  
-  
+  newsArray: INews[][]= []
   
   ngOnInit(): void {
+
     this.newsArray= [
-      {
+      [
+        {
         title: 'Explorando la Magia de Kioto, Japón',
         imageUrl: 'https://images.unsplash.com/photo-1545569341-9eb8b30979d9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         text: 'Kioto, la antigua capital de Japón, es un destino imprescindible para los amantes de la cultura y la historia. Sus templos milenarios, como el Fushimi Inari Taisha, sus hermosos jardines y sus calles empedradas hacen de esta ciudad un lugar encantador para perderse. No olvides probar un auténtico matcha en una de sus tradicionales casas de té.',
@@ -30,9 +30,10 @@ export class BlogComponent implements OnInit{
       text: 'La Patagonia argentina es un paraíso natural que ofrece impresionantes paisajes, desde glaciares hasta montañas nevadas y lagos de aguas cristalinas. Una caminata por el Parque Nacional Los Glaciares, hogar del famoso glaciar Perito Moreno, es una experiencia única que todo viajero debería vivir.',
       date: '12 de febrero de 2025',
     }
-    ];
+    
+    ]
+    ]
   }
-
 
   newTitle: string= "";
   newImage: string= "";
@@ -47,13 +48,19 @@ export class BlogComponent implements OnInit{
     }
   
     // Agregar la nueva noticia al array
-    this.newsArray.unshift({
+    const newsNew: INews= {
       title: this.newTitle,
       imageUrl: this.newImage,
       text: this.newText,
       date: this.newDate
-    });
-  
+    };
+
+    this.newsArray.unshift([
+      newsNew
+    ]
+
+    );
+
     // Limpiar los inputs después de agregar la noticia
     this.newTitle = "";
     this.newImage = "";
